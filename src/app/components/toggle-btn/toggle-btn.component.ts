9809d5bce67dce3ab88fbba1b81db7eeef26b46f
sunnycat000 <yanlily0111@gmail.com>
2021-06-05 16:40:34 -0700

    created toggle button. no function added yet

--- src/app/components/toggle-btn/toggle-btn.component.ts
+++ src/app/components/toggle-btn/toggle-btn.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-toggle-btn',
   templateUrl: './toggle-btn.component.html',
-  styleUrls: ['./toggle-btn.component.css']
+  styleUrls: ['./toggle-btn.component.css'],
 })
 export class ToggleBtnComponent implements OnInit {
+  text: string = 'test';
+  color: string = 'red';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  toggle() {
+    console.log('toggle');
   }
-
 }
