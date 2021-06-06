d65b26ffea811bce9c9fe6453096b9c41a413cee
sunnycat000 <yanlily0111@gmail.com>
2021-06-05 19:43:49 -0700

    Created task service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,13 +15,10 @@ import { NewTaskComponent } from './components/new-task/new-task.component';
     HeaderComponent,
     ToggleBtnComponent,
     TasksComponent,
-    NewTaskComponent
+    NewTaskComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/taks-service.service.ts
+++ src/app/services/taks-service.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../Task';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaksServiceService {
+  apiUrl: string = 'http://localhost:3000/tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  getTask(task: Task): Observable<Task> {
+    return this.http.get<Task>(`${this.http}/${task.id}`);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    return this.http.delete<Task>(`${this.http}/${task.id}`);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.patch<Task>(`${this.http}/${task.id}`, task);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(this.apiUrl, task);
+  }
 }
